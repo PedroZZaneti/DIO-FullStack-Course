4dabac5b66e7fc599efab995a305af645b43a518
Zaneti <pedrozzaneti@gmail.com>
2024-10-08 11:25:49 -0300

    Calculadora

---
+++ CalculadoraC#/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using CalculadoraC_.Models;
+
+Calculadora calc = new Calculadora();
+
+// calc.Somar(10 , 30);
+// calc.Subtrair(30, 20);
+// calc.Multiplicar(10, 5);
+// calc.Dividir(100, 20);
+calc.Potencia(3,3);
---
+++ EstruturasRepeticao/Program.cs
@@ -0,0 +1,23 @@
+﻿int numero = 0;
+int contador = 0;
+int soma = 0;
+// for (int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{contador}");
+//     contador++;
+// }
+
+do
+{
+    Console.WriteLine("Digite um numero");
+    numero = Convert.ToInt32(Console.ReadLine());
+    soma += numero;
+
+} while (numero!=0);
+
+Console.WriteLine($"O Total dos numeros é {soma}");
